7c244cfaafaf7470539c03a630b3847da10d545c
igdaniel-1 <igracedaniel@gmail.com>
2022-12-01 17:52:19 -0500

    photo saving works on web, new computer

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
